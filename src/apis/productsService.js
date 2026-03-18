8e3d87b4c82de4485f488535a0ace40d14d111bf
Tuan Anh <anhdt.cenland@gmail.com>
2026-03-18 21:29:38 +0700

    new create

--- src/apis/productsService.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from './axiosClient';
-
-const getProducts = async () => {
-    const res = await axiosClient.get('/product');
-
-    return res.data;
-};
-
-export { getProducts };
